2ff0196fa9d7efcc467123454328beca3006802c
vitalii-minchuk <mcmin4doutsch@gmail.com>
2022-01-18 13:53:35 +0200

    header done

--- pizza/script.js
+++ pizza/script.js
@@ -1,8 +1,6 @@
 
 console.log('hello')
 
-// ------------------------------------------------
-// так работает
 // let degrees = 0;
 
 
@@ -19,43 +17,4 @@ console.log('hello')
 // }
 // }
 // rotatePizza();
-
-// ------------------------
-// хочу сделать функцию, чтоб в неё передадь любую картинку и скорость и запускать по клику например
-// но не работает
-// let degrees = 0;
-// const rotateImg =  document.getElementById('bigPizza');
-
-// function rotatePizza(elem, speed) {
-//   elem.style.transform = `rotate(${degrees}deg)`;
-//   console.log(77)
-//   degrees++
-// setTimeout(rotatePizza, speed);
-// console.log(speed)
-// if (degrees > 359) {
-//   degrees = 1;
-// console.log(88)
-// }
-// }
-// rotatePizza(rotateImg, 25);
-
-
-
-let degrees = 0;
-const rotateImg =  document.getElementById('bigPizza');
-
-function rotatePizza(elem) {
-  elem.style.transform = `rotate(${degrees}deg)`;
-  console.log(77)
-  degrees++
-  console.log(elem)
-console.log(degrees)
-// setTimeout(rotatePizza(rotateImg), 25);
-console.log(speed)
-if (degrees > 359) {
-  degrees = 1;
-console.log(88)
-}
-}
-rotatePizza(rotateImg);
 
